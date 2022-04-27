a1762ff3b12ef725d3a0ad4238d703bf1cf4ba97
Eralia <eralia.katsakiori@outlook.com>
2022-04-27 18:35:38 +0300

    add event handlers for title + textBody + submit button - no functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,22 @@
+import { useState } from "react";
+
 function App(): JSX.Element {
+  const [title, setTitle] = useState("");
+  const [textBody, setTextBody] = useState("");
+
+  function handleTitle(title: string) {
+    setTitle(title);
+  }
+
+  function handleTextBody(textBody: string) {
+    setTextBody(textBody);
+  }
+
+  function handleSubmitPaste() {
+    //store the value of title and textBody
+    //send the values to the bakend
+  }
+
   return (
     <>
       <h1>Pastebin App</h1>
@@ -9,15 +27,21 @@ function App(): JSX.Element {
       </p>
       <br />
       <h3>Title (optional)</h3>
-      <input placeholder="Enter title here..." />
+      <input
+        placeholder="Enter title here..."
+        value={title}
+        onChange={(e) => handleTitle(e.target.value)}
+      />
       <h4>Enter your pastebin here:</h4>
       <textarea
         placeholder="Enter some text, this field cannot be empty."
         cols={60}
         rows={5}
+        value={textBody}
+        onChange={(e) => handleTextBody(e.target.value)}
       />
       <br />
-      <button>Submit</button>
+      <button onClick={handleSubmitPaste}>Submit</button>
       <br />
       <hr />
       <p>
